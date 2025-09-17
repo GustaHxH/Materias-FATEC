dd7822a6814e42b040e7eb3a8b0ac7e568fc7c0b
GustaHxH <gu.stavosantos8259@gmail.com>
2025-09-17 17:52:50 -0300

    termino atv 5

--- 4Semestre/Disp. Moveis I/Exercicios/src/ATV5/Ex065.kt
+++ 4Semestre/Disp. Moveis I/Exercicios/src/ATV5/Ex065.kt
@@ -1,2 +1,29 @@
 package ATV5
 
+class Funcionario(val nome:String, var salarioBruto:Double, val desconto:Double) {
+
+    fun salarioLiquido(): Double = salarioBruto - (salarioBruto * (desconto/100))
+    fun aumentarSalario(aumento: Double) {
+        salarioBruto += salarioBruto * aumento / 100.0
+    }
+}
+
+fun main(){
+    println("Qual o nome deste funcionário: ")
+    val nome: String = readLine()!!
+
+    println("Qual o salario bruto deste funcionário: ")
+    val salarioBruto: Double = readLine()?.toDoubleOrNull() ?: return println("Número inválido")
+
+    println("Qual o desconto do salário dele(número em %)")
+    val desconto: Double = readLine()?.toDoubleOrNull() ?: return println("Número inválido")
+
+    val func = Funcionario(nome, salarioBruto, desconto)
+    println("Funcionário: ${func.nome}, salário liquido: ${func.salarioLiquido()}")
+
+    println("Digite a % de aumento no salário")
+    val aumento:Double = readLine()?.toDoubleOrNull() ?: return println("Número inválido")
+
+    func.aumentarSalario(aumento)
+    println("Dados Atualizados: ${func.nome}, salario liquido = ${func.salarioLiquido()}")
+}
---
+++ 4Semestre/Disp. Moveis I/Exercicios/src/ATV6/Ex070.kt
@@ -0,0 +1,6 @@
+package ATV6
+
+fun main(){
+    val sub = {x: Int, y: Int -> x-y}
+    println(sub(8,9))
+}
